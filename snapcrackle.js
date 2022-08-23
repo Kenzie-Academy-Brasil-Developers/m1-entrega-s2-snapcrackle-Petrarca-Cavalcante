b60b0caa57b1827899624d8b1cc03ca0d7218726
Petrarca <pedropetrarcar@gmail.com>
2022-08-23 14:09:25 -0300

    update result.toString

--- snapcrackle.js
+++ snapcrackle.js
@@ -1,22 +1,24 @@
 function SnapCrackle(maxValue) {
     let result = []
-
+    let convertString = 0
     for(let i = 1; i < maxValue + 1; i++){
                 
         if (i % 2 == 0 && i % 5 != 0) {
-            result.push(i)
+            result.push(` ${i}`)
         }                
         if (i % 2 != 0 && i % 5 != 0) {
-            result.push("Snap")
+            result.push(" Snap")
         }
         if (i % 2 == 0 && i % 5 == 0) {
-            result.push("Crackle")
+            result.push(" Crackle")
         } 
         if (i % 2 != 0 && i % 5 == 0) {
-            result.push("SnapCrackle")
+            result.push(" SnapCrackle")
         }      
-    }   
-return result
+    }  
+
+    convertString = result.toString()
+return convertString
 }
 
-console.log(SnapCrackle(30))
+console.log(SnapCrackle(40))
